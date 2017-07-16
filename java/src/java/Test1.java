d727a993e112ac848d7f7362775528efd90b7965
xuenlai <1551058681@qq.com>
2017-07-16 16:30:46 +0900

    xel add

--- java/src/java/Test1.java
+++ java/src/java/Test1.java
@@ -3,6 +3,7 @@ package java;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("主分支内容");
+		System.out.println("次分之增加了内容");
 	}
 
 }
---
+++ java/src/java/Test2.java
@@ -0,0 +1,9 @@
+package java;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("次分支加了一个类");
+	}
+
+}
